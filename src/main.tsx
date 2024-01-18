5680ff9524f7b9aa10152d14cb54ded8cc5d3f95
skate090961 <m4xwork@yandex.ru>
2024-01-18 11:13:53 +0400

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+
+import { App } from '@/App'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
